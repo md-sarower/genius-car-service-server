092d2f92efc23fa1368a25a9fbb44e7e5d0e85ea
md-sarower <sarower9415@gmail.com>
2022-06-14 23:36:36 +0600

    Adding heroku some steps

---
+++ __server_deploy.js
@@ -0,0 +1,10 @@
+/* 
+* One time for your computer
+1. create heroku account
+2. verify email
+3. install heroku CLI
+4. heroku login
+--------------------------
+* For each project one time
+1. heroku create
+*/
